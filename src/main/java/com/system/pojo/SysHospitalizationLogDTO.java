fa3bc2bd39708b152162868893ee4f3d7419e520
lizongkai-big <lizongkai@aliyun.com>
2018-06-19 19:43:44 +0800

    增加住院信息表的日志记录功能：补充pojo文件SysHospitalizationLogDTO.java

---
+++ src/main/java/com/system/pojo/SysHospitalizationLogDTO.java
@@ -0,0 +1,42 @@
+package com.system.pojo;
+
+import java.util.Date;
+
+public class SysHospitalizationLogDTO {
+    Integer id;
+    Integer hid;
+    Date visitTime;
+    Date exitTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHid() {
+        return hid;
+    }
+
+    public void setHid(Integer hid) {
+        this.hid = hid;
+    }
+
+    public Date getVisitTime() {
+        return visitTime;
+    }
+
+    public void setVisitTime(Date visitTime) {
+        this.visitTime = visitTime;
+    }
+
+    public Date getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime(Date exitTime) {
+        this.exitTime = exitTime;
+    }
+}
