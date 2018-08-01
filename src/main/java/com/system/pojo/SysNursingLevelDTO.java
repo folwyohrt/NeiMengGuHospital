6cfafce4d5d2f8f60f0b41cce8b6a0d7d550d6b9
folwyohrt <lijingran@bupt.edu.cn>
2018-08-01 10:56:22 +0800

    护理的操作

---
+++ src/main/java/com/system/pojo/SysNursingLevelDTO.java
@@ -0,0 +1,25 @@
+package com.system.pojo;
+
+/**
+ * Create by henghhh on 2018.6.19
+ */
+public class SysNursingLevelDTO {
+    Integer id;
+    String value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
